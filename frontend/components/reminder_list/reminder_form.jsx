d476f1699a65e9b5ed40c578aa88cc858e69a6de
holyoke <jasonebueng@gmail.com>
2016-12-01 11:38:01 -0800

    adds date picking for remind date

--- frontend/components/reminder_list/reminder_form.jsx
+++ frontend/components/reminder_list/reminder_form.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import uniqueId from '../../util/id_generator'
 import moment from 'moment'
+import DatePicker from 'react-datepicker'
+
+import 'react-datepicker/dist/react-datepicker.css'
 
 class ReminderForm extends React.Component {
   constructor (props) {
@@ -9,10 +12,11 @@ class ReminderForm extends React.Component {
       title: '',
       body: '',
       done: false,
-      remind_date: moment().add(24, 'hours').format('LLL')
+      remind_date: moment().add(24, 'hours')
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   update (property) {
@@ -22,12 +26,22 @@ class ReminderForm extends React.Component {
   handleSubmit (e) {
     e.preventDefault()
     const reminder = Object.assign({}, this.state, {id: uniqueId()})
+
+    //parse date
+    reminder.remind_date = reminder.remind_date.format("LLL")
     this.props.receiveReminder(reminder)
-    
+
     // reset form
-    this.setState ({
+    this.setState({
       title: '',
       body: '',
+      remind_date: moment().add(24, 'hours')
+    })
+  }
+
+  handleChange (date) {
+    this.setState({
+      remind_date: date
     })
   }
 
@@ -53,6 +67,11 @@ class ReminderForm extends React.Component {
             onChange={this.update('body')}
             required />
         </label>
+
+        <link rel='stylesheets' href='dist/react-datepicker.css' />
+        <DatePicker
+        selected={this.state.remind_date}
+        onChange={this.handleChange} />
 
         <button className="create-button">Create reminder!</button>
       </form>
